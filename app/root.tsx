77613ef49d67eaabce6af87f33ceac6dd61c9688
Storm K <stormk@hotmail.dk>
2022-05-15 22:14:44 +0200

    bootstrapped remix app with tailwind, eslint, prettier & mantine ui

---
+++ app/root.tsx
@@ -0,0 +1,38 @@
+import type { ColorScheme } from '@mantine/core';
+import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { useState } from 'react';
+import tailwind from './tailwind.css';
+
+export const meta: MetaFunction = () => ({
+  charset: 'utf-8',
+  title: 'Smartender',
+  viewport: 'width=device-width,initial-scale=1',
+});
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: tailwind }];
+
+export default function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+  const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <html lang='en'>
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+          <MantineProvider theme={{ colorScheme }} withNormalizeCSS withGlobalStyles>
+            <Outlet />
+          </MantineProvider>
+        </ColorSchemeProvider>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
